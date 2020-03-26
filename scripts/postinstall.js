a662cbd2678b32ddba7482923ddcdb6e257c5c4c
Flavian Desverne <flavian.desverne@epitech.eu>
2020-03-26 14:10:21 +0100

    chore: add postinstall script to help (#66)

---
+++ scripts/postinstall.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk')
+
+console.log(chalk.bold.yellowBright('----------------------------------'))
+console.log(
+    chalk.bold.yellowBright(
+      `If you want to learn more about the ${chalk.reset.greenBright(
+        `\`nexus-plugin-prisma\``
+      )}`
+    )
+)
+console.log(
+  chalk.bold.yellowBright(
+    `Follow this link: ${chalk.reset.greenBright(`http://nxs.li/nexus-plugin-prisma`)}`
+  )
+)
+console.log(chalk.bold.yellowBright('----------------------------------'))
