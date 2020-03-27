90ceb7a9259b4971ceb905da0664e19fddf7925e
William Luke <william@atto-byte.com>
2020-03-27 20:25:36 +0300

    fix: postinstall script on Windows

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,20 @@
 const chalk = require('chalk')
+const jetpack = require('fs-jetpack');
+const path = require('path');
+// proccess.env.PWD is undifined on Windows https://github.com/mrblueblue/gettext-loader/issues/18
+const pwd = process.cwd();
+const destDir = path.join(pwd, '..', '@types', 'nexus-plugin-prisma')
+
+jetpack.dir(destDir)
+jetpack.copy(path.join(pwd, 'global-type.d.ts'), path.join(destDir, 'index.d.ts'), { overwrite: true })
 
 console.log(chalk.bold.yellowBright('----------------------------------'))
 console.log(
-    chalk.bold.yellowBright(
-      `If you want to learn more about the ${chalk.reset.greenBright(
-        `\`nexus-plugin-prisma\``
-      )}`
-    )
+  chalk.bold.yellowBright(
+    `If you want to learn more about the ${chalk.reset.greenBright(
+      `\`nexus-plugin-prisma\``
+    )}`
+  )
 )
 console.log(
   chalk.bold.yellowBright(
